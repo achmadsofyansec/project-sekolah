fd92ddb9f0bad2d60ea3a765f0b925e4ce79b000
Koichiro12 <choiruldhani889@gmail.com>
2022-07-15 15:21:20 +0700

    newYgy

--- db/sekolahapp.sql
+++ db/sekolahapp.sql
@@ -1,6 +1,6 @@
 /*
-SQLyog Community v13.1.7 (64 bit)
-MySQL - 10.5.9-MariaDB : Database - sekolahapp
+SQLyog Community v13.0.1 (64 bit)
+MySQL - 10.6.5-MariaDB : Database - sekolahapp
 *********************************************************************
 */
 
@@ -39,7 +39,7 @@ CREATE TABLE `migrations` (
   `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `batch` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 /*Data for the table `migrations` */
 
@@ -47,7 +47,9 @@ insert  into `migrations`(`id`,`migration`,`batch`) values
 (21,'2014_10_12_000000_create_users_table',1),
 (22,'2014_10_12_100000_create_password_resets_table',1),
 (23,'2019_08_19_000000_create_failed_jobs_table',1),
-(24,'2019_12_14_000001_create_personal_access_tokens_table',1);
+(24,'2019_12_14_000001_create_personal_access_tokens_table',1),
+(27,'2022_07_14_015617_create_roles_table',2),
+(28,'2022_07_15_064050_sekolah',3);
 
 /*Table structure for table `password_resets` */
 
@@ -83,6 +85,47 @@ CREATE TABLE `personal_access_tokens` (
 
 /*Data for the table `personal_access_tokens` */
 
+/*Table structure for table `roles` */
+
+DROP TABLE IF EXISTS `roles`;
+
+CREATE TABLE `roles` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `roles_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `roles_access` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `id_roles` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+/*Data for the table `roles` */
+
+/*Table structure for table `sekolah` */
+
+DROP TABLE IF EXISTS `sekolah`;
+
+CREATE TABLE `sekolah` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `kode_sekolah` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `nsm` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `npsn` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `akreditasi` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `nama_sekolah` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `jenjang` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `alamat` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `website` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `kode_pos` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `nomor_hp` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `sekolah_email_unique` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+/*Data for the table `sekolah` */
+
 /*Table structure for table `users` */
 
 DROP TABLE IF EXISTS `users`;
@@ -100,12 +143,13 @@ CREATE TABLE `users` (
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `users_email_unique` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 /*Data for the table `users` */
 
 insert  into `users`(`id`,`name`,`email`,`email_verified_at`,`password`,`role`,`id_role`,`remember_token`,`created_at`,`updated_at`) values 
-(1,'Administrator','admin@sipinter.com',NULL,'$2y$10$J5QhqMQTJpdcYAL9zgetsOzi3N0j5n0uxu2hNY6uYX/GGXDRvYoMa','admin','0',NULL,NULL,NULL);
+(1,'Administrator','admin@sipinter.com',NULL,'$2y$10$J5QhqMQTJpdcYAL9zgetsOzi3N0j5n0uxu2hNY6uYX/GGXDRvYoMa','admin','0',NULL,NULL,NULL),
+(2,'Administrator','admin@jamanu.com',NULL,'$2y$10$otWU8EbYYO5FOhS3lvCUUeWJ6HKASiUk6wloI8WDOAdlkNABKXeNC','admin','0',NULL,NULL,NULL);
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
