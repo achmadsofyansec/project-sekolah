56a445eacd55ef34be1c98d99a9b4eda884d6739
Koichiro12 <choiruldhani889@gmail.com>
2022-08-13 12:43:38 +0700

    First Commit

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -19,6 +19,50 @@
     </div>
     <!-- Main content -->
     <section class="content">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-12 col-sm-6 col-md-3">
+            <a href="#"style="color:black;">
+            <div class="info-box shadow-lg">
+              <span class="info-box-icon bg-danger elevation-1"><i class="fas fa-user-graduate"></i></span>
+              <div class="info-box-content">
+                <span class="info-box-text text-danger" ><b>Alumni</b></span>
+                <font  style="text-shadow: 2px 2px 4px #827e7e">0</font>
+              </div>
+            </div></a>
+          </div>
+          <div class="col-12 col-sm-6 col-md-3">
+            <a href="#" style="color:black;">
+            <div class="info-box shadow-lg">
+              <span class="info-box-icon bg-primary elevation-1"><i class="fas fa-people-carry"></i></span>
+              <div class="info-box-content">
+                <span class="info-box-text text-primary" ><b>Lowongan</b></span>
+                <font style="text-shadow: 2px 2px 4px #827e7e">0</font>
+              </div>
+            </div></a>
+          </div>
+          <div class="col-12 col-sm-6 col-md-3">
+            <a href="#"style="color:black;">
+            <div class="info-box shadow-lg">
+              <span class="info-box-icon bg-success elevation-1"><i class="fas fa-copy"></i></span>
+              <div class="info-box-content">
+                <span class="info-box-text text-success" ><b>Pendaftaran</b></span>
+                <font style="text-shadow: 2px 2px 4px #827e7e">0</font>
+              </div>
+            </div></a>
+          </div>
+          <div class="col-12 col-sm-6 col-md-3">
+            <a href="#" style="color:black;">
+            <div class="info-box shadow-lg">
+              <span class="info-box-icon bg-info elevation-1"><i class="fas fa-paste"></i></span>
+              <div class="info-box-content">
+                <span class="info-box-text text-info" ><b>Pengumuman</b></span>
+                <font style="text-shadow: 2px 2px 4px #827e7e">0</font>
+              </div>
+            </div></a>
+          </div>
+        </div>
+      </div>
     </section>
     <!-- /.content -->
   </div>
