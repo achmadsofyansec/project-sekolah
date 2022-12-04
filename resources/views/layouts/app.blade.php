c1bf4c5adf69b401bf4a5ae41653ada8276740dd
abogoboga02 <anggamdk4@gmail.com>
2022-12-04 10:08:17 +0700

    p

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -82,7 +82,7 @@
   <aside class="main-sidebar sidebar-light-pink elevation-4">
     <!-- Brand Logo -->
     <a href="<?php echo url('/') ?>" class="brand-link">
-      <img src="{{asset('public/dist/img/AdminLTELogo.png')}}" alt="Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <img src="{{asset('public/dist/img/jamanu_logo.png')}}" alt="Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">ALUMNI</span>
     </a>
 
@@ -91,7 +91,7 @@
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="{{asset('public/dist/img/user2-160x160.jpg')}}" class="img-circle elevation-2" alt="User Image">
+          <img src="{{asset('public/dist/img/jamanu_logo.png')}}" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
           <a href="#" class="d-block"><?php echo strtoupper(auth()->user()->name);?></a>
